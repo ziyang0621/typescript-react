212418c81a58f725aa5c3612b1a13a2282f71ffa
Ziyang Tan <ziyang0621@gmail.com>
2019-02-14 19:21:02 -0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+
+import CounterOutput from './CounterOutput';
+
+interface IAppState {
+  counterValue: number;
+}
+
+class App extends React.Component<{}, IAppState> {
+  public state = {counterValue: 0};
+
+  public render() {
+    return (
+      <div style={{ textAlign: 'center' }}>
+        <CounterOutput counter={this.state.counterValue} />
+        <button onClick={this.incHandler}>Increment</button>
+        <button onClick={this.decHandler}>Decrement</button>
+      </div>
+    );
+  }
+
+  private incHandler = () => {
+    this.setState(prevState => {
+      return { counterValue: prevState.counterValue + 1 };
+    });
+  }
+
+  private decHandler = () => {
+    this.setState(prevState => {
+      return { counterValue: prevState.counterValue - 1 };
+    });
+  }
+}
+
+export default App;
